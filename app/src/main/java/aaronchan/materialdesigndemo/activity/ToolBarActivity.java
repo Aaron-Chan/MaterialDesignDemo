8ec1f9ee1b0b7a79b66b4b6e2bec20145157fa17
chenxiaocheng <1903682265@qq.com>
2016-09-08 00:40:19 +0800

    add ToolBarActivity not finish ToolBar Demo

---
+++ app/src/main/java/aaronchan/materialdesigndemo/activity/ToolBarActivity.java
@@ -0,0 +1,16 @@
+package aaronchan.materialdesigndemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import aaronchan.materialdesigndemo.R;
+
+public class ToolBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+
+    }
+}
